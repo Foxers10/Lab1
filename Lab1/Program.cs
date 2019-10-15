813dd4ec6bb489345927a26e63d5ef543fadb355
Лисин Андрей <foxers100@gmail.com>
2019-10-15 23:03:35 +0300

    start

---
+++ Lab1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static int a, b, c;
+        static double d, x1, x2, xk1, xk2;
+        static void Main(string[] args)
+        {
+            Console.Title = "Лисин Андрей РТ5-31Б";
+            try
+            {
+                Console.WriteLine("Введите a, b, c");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                c = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка ввода");
+                Console.ReadKey();
+            }
+            d = b*b - 4 * a * c;
+            Console.WriteLine(d);
+            if (d > 0)
+            {
+                xk1 = (-b + Math.Sqrt(d)) / (2 * a);
+                xk2 = (-b - Math.Sqrt(d)) / (2 * a);
+                if (xk1 < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("действительных корней нет");
+                }
+                else
+                {
+                    x1 = Math.Sqrt(xk1);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("корень х1= +-");
+                    Console.WriteLine(x1);
+                    if (xk2 < 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("второго корня нет");
+                    }
+                    else
+                    {
+                        x2 = Math.Sqrt(xk2);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("корень х2= +-");
+                        Console.WriteLine(x2);
+                    }
+                }
+            }
+            else if (d == 0)
+            {
+                xk1 = (-b) / (2 * a);
+                if (xk1 < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("действительных корней нет");
+                }
+                else
+                {
+                    x1 = Math.Sqrt(xk1);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("корень х= +-", x1);
+                    Console.WriteLine(x1);
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("действительных корней нет");
+            }
+            Console.ReadKey();
+        }
+    }
+}
